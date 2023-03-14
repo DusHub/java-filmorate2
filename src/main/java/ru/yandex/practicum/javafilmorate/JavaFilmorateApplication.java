338a0f72ee8b030165f0009b6dc1dd3d582444ea
Masfuj <gooddayemail@gmail.com>
2023-03-14 15:54:17 +0300

    test commit #1

--- src/main/java/ru/yandex/practicum/javafilmorate/JavaFilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/javafilmorate/JavaFilmorateApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaFilmorateApplication {
 
-
+	//comment for test push
 	public static void main(String[] args) throws JsonProcessingException {
 		SpringApplication.run(JavaFilmorateApplication.class, args);
 	}
